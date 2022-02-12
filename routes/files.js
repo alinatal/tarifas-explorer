1229caa455596b42c020538d69d23d1acfc37788
cristiancosano <cristiancosano@icloud.com>
2022-02-13 00:39:24 +0100

    fourth commit

--- routes/files.js
+++ routes/files.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
+const path = require('path');
 const getFiles = require('../common/getFiles');
 const router = Router();
 
 router.get('', (req, res) => {
-    res.send({files: getFiles('tarifas')})
+    res.send({files: getFiles(path.join(__dirname, '../tarifas'))})
 });
 
 module.exports = router;
