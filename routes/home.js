10f62d5d172fc32d56d8d955c0ba282874e2ca2e
cristiancosano <cristiancosano@icloud.com>
2022-02-13 00:32:59 +0100

    second commit

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,7 @@ const path = require('path');
 const router = Router();
 
 router.get('', (req, res) => {
-    res.sendFile(path.resolve('views/index.html'))
+    res.sendFile(path.join(__dirname, '../views/index.html'))
 })
 
 module.exports = router;
